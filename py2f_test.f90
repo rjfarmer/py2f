b189056133afe960da7a7d8b02df6eec5694e502
Robert Farmer <rjfarmer@asu.edu>
2016-01-22 16:22:27 -0700

    Begin rewriting test framework

--- py2f_test.f90
+++ py2f_test.f90
@@ -1,80 +1,71 @@
-
-PROGRAM main
+MODULE tester
    use py2f
    IMPLICIT NONE
-   INTEGER :: x,res,res1,res2
-   INTEGER(8) :: y
-   INTEGER(8) :: xx=1
-   CHARACTER(len=256) :: s
-   DOUBLE PRECISION :: aa,bb
-   DOUBLE PRECISION,DIMENSION(1:6) :: xarr
-   DOUBLE PRECISION,DIMENSION(2,3) :: xarr2
-   DOUBLE PRECISION,DIMENSION(:),ALLOCATABLE :: yarr
-   DOUBLE PRECISION,DIMENSION(:,:),ALLOCATABLE :: yarr2
    
-   aa=10.d0
+   CONTAINS
    
-   res=setup()
-   if(res/=SUCCESS) THEN
-      write(*,*) "Init failed"
-      stop
-   end if
-   res=run_cmd("import numpy")
-   if(res/=SUCCESS) THEN
-      write(*,*) "Load failed"
-      stop
-   end if
-   !x=run_cmd("import numpy as np")
-   res=set(MAIN_MOD,"zz","abc")
-    write(*,*) res
-    if(res/=SUCCESS) stop
-    
-   res=get("numpy","__version__",s)
-    write(*,*) res,trim(s)
-    if(res/=SUCCESS) stop
-   
-   res1=set("numpy","xxxxxxxxxxxx",xx)
-   write(*,*) "res1",res1
-   if(res1/=SUCCESS) stop
-   
-   res2=get("numpy","xxxxxxxxxxxx",y)
-   if(res2/=SUCCESS) stop
-   write(*,*) "**",y,res1,res2
-   
-   res1=set(MAIN_MOD,"yy",aa)
-   if(res1/=SUCCESS) stop
-   
-   res2=get(MAIN_MOD,"yy",bb)
-   if(res2/=SUCCESS) stop
-   write(*,*) bb,res1,res2   
-   
-   xarr=(/1.d0,2.d0,3.d0,4.d0,5.d0,6.d0/)
-   
-   res2=set(MAIN_MOD,"xarr",xarr)
-   if(res2/=SUCCESS) stop    
-   
-   
-   xarr2=reshape(xarr,(/2,3/))
-   res2=set(MAIN_MOD,"xarr",xarr2)
-   if(res2/=SUCCESS) stop   
-   
-   res=run_cmd("print xarr")
-   
-   
-   res2=get(MAIN_MOD,"xarr",yarr)
-   if(res2/=SUCCESS) stop
-   write(*,*) "yarr ",res2,xarr,"*",yarr ,"*",shape(yarr)
+   SUBROUTINE check(res,func)
+      IMPLICIT NONE
+      INTEGER :: res
+      CHARACTER(LEN=*) :: func
+      write(*,'(A)',advance='no') "Testing "//func//" "
+      if(res==SUCCESS)THEN
+         write(*,*) "Passed"
+      else
+         write(*,*) "Failed"
+         stop
+      end if
+   END SUBROUTINE check
 
-!    res2=get(MAIN_MOD,"xarr",yarr2)
-!    if(res2/=SUCCESS) stop
-!    write(*,*) "yarr2 ",res2,xarr,"*",yarr2 ,"*",shape(yarr2)   
+   SUBROUTINE test_setup()
+      IMPLICIT NONE
+
+      call check(setup(),"setup")
+         
+   END SUBROUTINE test_setup
  
-   res2=dealloc(MAIN_MOD,"xarr")
-   if(res2/=SUCCESS) stop
-    res=run_cmd("print xarr")
-!    
-    res2=run_cmd("x=1;print x")
+   SUBROUTINE test_finish()
+      IMPLICIT NONE
 
-   x=finish()
+      call check(finish(),"finish")
+         
+   END SUBROUTINE test_finish
    
+   SUBROUTINE test_set_int()
+      IMPLICIT NONE
+
+      call check(set(MAIN_MOD,"x",1),"set_int")
+         
+   END SUBROUTINE test_set_int  
+   
+   SUBROUTINE test_set_dble()
+      IMPLICIT NONE
+
+      call check(set(MAIN_MOD,"x",1.d0),"set_dble")
+         
+   END SUBROUTINE test_set_dble 
+   
+   SUBROUTINE test_set_str()
+      IMPLICIT NONE
+
+      call check(set(MAIN_MOD,"x","1"),"set_str")
+         
+   END SUBROUTINE test_set_str  
+   
+
+END MODULE tester
+
+
+
+PROGRAM main
+   use tester
+   IMPLICIT NONE
+   
+   call test_setup()
+   call test_set_int()
+   call test_set_dble()
+   call test_set_str()
+   
+   call test_finish()
+
 END PROGRAM main
